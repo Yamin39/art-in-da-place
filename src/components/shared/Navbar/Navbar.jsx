eae2a722523075d7d509849c576d38eda873f922
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-09-04 14:30:10 +0600

    added navbar

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import { IoClose } from "react-icons/io5";
+import { MdOutlineMenu } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/pages">Pages</NavLink>
+      </li>
+      <li>
+        <NavLink to="/gallery">Gallery</NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog">Blog</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+      <li>
+        <NavLink to="/shop">Shop</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <header className="max-w-[1440px] w-[72%] mx-auto pt-7">
+      <nav className="flex justify-between items-center">
+        <div className="max-w-[9.375rem]">
+          <img className="w-full" src={logo} alt="Logo" />
+        </div>
+
+        {/* desktop navigation */}
+        <ul className="desk-nav hidden md:flex gap-[3.125rem] pr-5">{navLinks}</ul>
+
+        {/* mobile / tablet navigation */}
+        <div className="drawer drawer-end md:hidden">
+          <input id="side-nav" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <label htmlFor="side-nav" className="flex items-center btn btn-ghost text-base font-normal">
+              <MdOutlineMenu className="text-3xl" />
+              Menu
+            </label>
+          </div>
+          <div className="drawer-side">
+            <label htmlFor="side-nav" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu mobile-nav bg-black text-white min-h-full w-80 p-4">
+              {/* Sidebar content here */}
+              <li className="mb-2">
+                <label htmlFor="side-nav" aria-level="close sidebar" className="w-fit ml-auto">
+                  <IoClose className="text-2xl" />
+                </label>
+              </li>
+              {navLinks}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar/Navbar";
 
 const Root = () => {
   return (
-    <div className="font-fira-sans">
+    <div className="font-fira-sans bg-black text-white">
+      <Navbar />
       <Outlet />
     </div>
   );
