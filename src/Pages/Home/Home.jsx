f42120be929169ccf29efc4c6365da5040237272
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-09-04 13:09:52 +0600

    init

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="flex h-screen justify-center items-center font-bold text-4xl underline text-secondary-color">Hello World</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "secondary-color": "#A81C51",
+      },
+      fontFamily: {
+        josefin: `"Josefin Sans", sans-serif`,
+        "fira-sans": `"Fira Sans Condensed", sans-serif`,
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
