40e98baf818ab3e197352f0b9aa863628904cfbc
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-09-08 02:21:31 +0600

    updated footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import { FaSquareFull } from "react-icons/fa";
+import { FaComment } from "react-icons/fa6";
+import { LuClock3, LuLink } from "react-icons/lu";
 import { RxBoxModel } from "react-icons/rx";
 import recentArt1 from "../../../assets/recent/recent-1.jpg";
 import recentArt2 from "../../../assets/recent/recent-2.jpg";
@@ -8,8 +10,16 @@ import recentArt5 from "../../../assets/recent/recent-5.jpg";
 import recentArt6 from "../../../assets/recent/recent-6.jpg";
 
 const Footer = () => {
-  // 11 random art gallery website tags for the footer
   const tags = ["Abstract", "Art", "Artwork", "Canvas", "Design", "Gallery", "Illustration", "Modern", "Painting", "Photography", "Sculpture"];
+  const recentImages = [recentArt1, recentArt2, recentArt3, recentArt4, recentArt5, recentArt6];
+
+  // random 3 recent news with title, date, and comment count. title will be "News_title", date will be "17 Nov", comment count will be 0
+  const recentNews = [
+    { id: 1, title: "Quisque egestas rhoncus mauris", date: "17 Nov", comment: 0 },
+    { id: 2, title: "Integer sed condimentum lacus", date: "17 Nov", comment: 0 },
+    { id: 3, title: "Nam sodales tincidunt nunc", date: "17 Nov", comment: 1 },
+  ];
+
   return (
     <footer>
       <div className="flex gap-3 items-center justify-center mx-auto py-4">
@@ -35,7 +45,7 @@ const Footer = () => {
         <div className="hidden md:block w-[33%] h-[0.05rem] bg-[#464646d7]"></div>
       </div>
 
-      <div className="max-w-[73.125rem] w-11/12 md:w-10/12 mx-auto grid gap-[1.875rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-12">
+      <div className="max-w-[73.125rem] w-11/12 md:w-10/12 mx-auto grid gap-[1.875rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-12">
         <div>
           <div>
             <div className="w-[1.875rem] h-[0.125rem] bg-secondary-color"></div>
@@ -56,24 +66,16 @@ const Footer = () => {
           </div>
 
           <div className="grid gap-2 grid-cols-3">
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt1} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt2} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt3} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt4} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt5} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
-            <div className="overflow-hidden max-h-[5.2131rem]">
-              <img src={recentArt6} className="size-full cursor-pointer hover:scale-105 duration-300 object-cover" />
-            </div>
+            {recentImages.map((image, index) => (
+              <div key={index} className="relative max-h-[5.2131rem]">
+                <div className="absolute flex justify-center items-center size-full bg-secondary-color opacity-0 hover:opacity-100 duration-300">
+                  <a href="#" className="text-white text-xl p-2">
+                    <LuLink />
+                  </a>
+                </div>
+                <img src={image} className="size-full cursor-pointer duration-300 object-cover" />
+              </div>
+            ))}
           </div>
         </div>
 
@@ -92,6 +94,34 @@ const Footer = () => {
               >
                 {tag}
               </a>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div className="w-[1.875rem] h-[0.125rem] bg-secondary-color"></div>
+            <h4 className="mt-4 mb-10 font-josefin text-lg tracking-[0.0313rem] leading-[1.2] uppercase">Recent News</h4>
+          </div>
+
+          <div className="space-y-3">
+            {recentNews.map((news) => (
+              <div key={news?.id} className="pb-[0.9375rem] mb-[0.625rem] border-b-[0.1875rem] border-double border-[#333333] duration-300 hover:border-white">
+                <a href="#" className="mb-[0.4375rem] text-sm leading-5 uppercase font-josefin tracking-[0.0156rem] hover:text-secondary-color duration-300">
+                  {news?.title}
+                </a>
+
+                <ul className="text-[0.8125rem] text-[#ffffff4d] flex items-center gap-4 mt-2">
+                  <li className="flex items-center gap-3">
+                    <LuClock3 />
+                    <p>{news?.date}</p>
+                  </li>
+                  <li className="flex items-center gap-3">
+                    <FaComment />
+                    <p>{news?.comment}</p>
+                  </li>
+                </ul>
+              </div>
             ))}
           </div>
         </div>
