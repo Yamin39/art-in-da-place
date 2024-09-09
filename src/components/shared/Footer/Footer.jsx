e0625124bdfb1becf99dbb5de2730884c514e23a
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-09-09 16:53:20 +0600

    removed recent news from footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import { FaSquareFull } from "react-icons/fa";
-import { FaComment } from "react-icons/fa6";
-import { LuClock3, LuLink } from "react-icons/lu";
+import { LuLink } from "react-icons/lu";
 import { RxBoxModel } from "react-icons/rx";
 import recentArt1 from "../../../assets/recent/recent-1.jpg";
 import recentArt2 from "../../../assets/recent/recent-2.jpg";
@@ -12,11 +11,6 @@ import recentArt6 from "../../../assets/recent/recent-6.jpg";
 const Footer = () => {
   const tags = ["Abstract", "Art", "Artwork", "Canvas", "Design", "Gallery", "Illustration", "Modern", "Painting", "Photography", "Sculpture"];
   const recentImages = [recentArt1, recentArt2, recentArt3, recentArt4, recentArt5, recentArt6];
-  const recentNews = [
-    { id: 1, title: "Quisque egestas rhoncus mauris", date: "17 Nov", comment: 0 },
-    { id: 2, title: "Integer sed condimentum lacus", date: "17 Nov", comment: 0 },
-    { id: 3, title: "Nam sodales tincidunt nunc", date: "17 Nov", comment: 1 },
-  ];
 
   return (
     <footer>
@@ -43,7 +37,7 @@ const Footer = () => {
         <div className="hidden md:block w-[33%] h-[0.05rem] bg-[#464646d7]"></div>
       </div>
 
-      <div className="max-w-[73.125rem] w-11/12 md:w-10/12 mx-auto grid gap-[1.875rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mt-12">
+      <div className="max-w-[73.125rem] w-11/12 md:w-10/12 mx-auto grid gap-[1.875rem] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-12">
         <div>
           <div>
             <div className="w-[1.875rem] h-[0.125rem] bg-secondary-color"></div>
@@ -92,34 +86,6 @@ const Footer = () => {
               >
                 {tag}
               </a>
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <div className="w-[1.875rem] h-[0.125rem] bg-secondary-color"></div>
-            <h4 className="mt-4 mb-10 font-josefin text-lg tracking-[0.0313rem] leading-[1.2] uppercase">Recent News</h4>
-          </div>
-
-          <div className="space-y-3">
-            {recentNews.map((news) => (
-              <div key={news?.id} className="pb-[0.9375rem] mb-[0.625rem] border-b-[0.1875rem] border-double border-[#333333] duration-300 hover:border-white">
-                <a href="#" className="mb-[0.4375rem] text-sm leading-5 uppercase font-josefin tracking-[0.0156rem] hover:text-secondary-color duration-300">
-                  {news?.title}
-                </a>
-
-                <ul className="text-[0.8125rem] text-[#ffffff4d] flex items-center gap-4 mt-2">
-                  <li className="flex items-center gap-3">
-                    <LuClock3 />
-                    <p>{news?.date}</p>
-                  </li>
-                  <li className="flex items-center gap-3">
-                    <FaComment />
-                    <p>{news?.comment}</p>
-                  </li>
-                </ul>
-              </div>
             ))}
           </div>
         </div>
